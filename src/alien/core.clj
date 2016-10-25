07697ffc36895222f52188c62ff95a008f795fd2
Mathew Prokos <mprokos@meraki.com>
2016-10-25 16:05:46 -0700

    added num-logs - WIP

--- src/alien/core.clj
+++ src/alien/core.clj
@@ -2,12 +2,28 @@
   (:gen-class)
   (require clojure.string))
 
+(defn logn [base n]
+  (/ (Math/log n) (Math/log base)))
+
 (defn to-native [{:keys [value table]}]
   (let [base (count table)]
     (map-indexed (fn [c index] (* (clojure.string/index-of table c) (bigint (Math/pow base index))))) value))
 
+(defn to-alien [{:keys [value table]}]
+  1)
+
+(defn num-logs
+  ([value base] (num-logs value base 0)) 
+  ([value base total]
+   (if (<= value 1)
+     total
+     (let [v (bigint (logn base value))]
+       (println value base total)
+       (num-logs (- value v) base (+ value v))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (to-native {:value "110" :table "0123456789"}) 
+  (to-native {:value "110" :table "0123456789"})
+  (logn 2 1000)
+  (num-logs 1000 2) 
   (println "Hello, World!"))
